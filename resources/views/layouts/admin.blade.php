b908e7d1fa28e1f5811643a57a43a7b14afd20f5
Ankit Tiwari <ankittiwaari@gmail.com>
2021-03-27 00:41:14 +0530

    Admin cleanup

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,15 +17,10 @@
     <link rel="stylesheet" href="{{ asset('css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- iCheck -->
     <link rel="stylesheet" href="{{ asset('css/icheck-bootstrap.min.css') }}">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="{{ asset('css/jqvmap.min.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('css/adminlte.min.css')}}">
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="{{ asset('css/OverlayScrollbars.min.css')}}">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{ asset('css/daterangepicker.css')}}">
-    <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('css/summernote-bs4.min.css')}}">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -94,25 +89,8 @@
 
 <!-- jQuery -->
 <script src="{{ asset('js/jquery.min.js') }}"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="{{ asset('js/jquery-ui.min.js')}}"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button)
-</script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('js/bootstrap.bundle.min.js')}}"></script>
-<!-- ChartJS -->
-<script src="{{ asset('js/Chart.min.js')}}"></script>
-<!-- Sparkline -->
-<script src="{{ asset('js/sparkline.js')}}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{ asset('js/jquery.knob.min.js') }}"></script>
-<!-- daterangepicker -->
-<script src="{{ asset('js/moment.min.js') }}"></script>
-<script src="{{ asset('js/daterangepicker.js') }}"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ asset('js/tempusdominus-bootstrap-4.min.js') }}"></script>
 <!-- Summernote -->
 <script src="{{ asset('js/summernote-bs4.min.js') }}"></script>
 <!-- overlayScrollbars -->
